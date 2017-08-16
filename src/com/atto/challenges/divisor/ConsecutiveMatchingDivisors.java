9fffee9c07e76838dd11d195ff97c5b84629f867
Anderson Yamamoto <attogba@gmail.com>
2017-08-16 00:56:05 -0300

    Minor performance improvements. Needs work.

--- src/com/atto/challenges/divisor/ConsecutiveMatchingDivisors.java
+++ src/com/atto/challenges/divisor/ConsecutiveMatchingDivisors.java
@@ -1,6 +1,6 @@
 package com.atto.challenges.divisor;
 
-// Find the number of integers 1 < n < 107, for which n and n + 1 have the 
+// Find the number of integers 1 < n < 10^7, for which n and n + 1 have the 
 // same number of positive divisors. 
 // For example, 14 has the positive divisors 1, 2, 7, 14 while 15 has 1, 3, 5, 15.
 public class ConsecutiveMatchingDivisors
@@ -21,10 +21,20 @@ public class ConsecutiveMatchingDivisors
     
     public static int numberOfDivisors(int number){
         int numberDivisors = 2;
-        for (int i = 2; i <= number/2; i++){
-            if (number%i == 0)
-                numberDivisors++;
+        boolean even = number%2 == 0;
+        if (even){
+            numberDivisors += 2;
+            for (int i = 3; i <= number/3; i++){
+                if (number%i == 0)
+                    numberDivisors++;
+            }
+        }else{
+            for (int i = 3; i <= number/3; i+= 2){
+                if (number%i == 0)
+                    numberDivisors++;
+            }
         }
+        
         return numberDivisors;
-    }
+    }//need to decompose into prime multipliers and calculate combinations instead, should be faster
 }
