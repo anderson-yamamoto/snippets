63a9d2120f568e34ed9c48c055bc0f9675e64084
Anderson Yamamoto <attogba@gmail.com>
2017-08-16 22:02:16 -0300

    Challenges: https://projecteuler.net/problem=38

--- src/com/atto/challenges/EulerProject.java
+++ src/com/atto/challenges/EulerProject.java
@@ -5,7 +5,7 @@ public class EulerProject
     public static void main(String[] args)
     {
         
-        System.out.println(multiples3and5());
+        System.out.println(pandigital());
         
     }
     
@@ -27,5 +27,45 @@ public class EulerProject
             difference += (sum - i) * i;
         }
         return difference;
+    }
+    
+    //https://projecteuler.net/problem=38
+    public static int pandigital(){
+        int biggest = 0, biggestIntGenerated = 0;
+        String biggestPandigital = "";
+        for (int i = 9999; i > 1; i--)
+        {
+            StringBuilder pandigital = new StringBuilder();
+            int multiplier = 1;
+            pandigital.append(i*multiplier);
+            while (pandigital.length() < 9){
+                multiplier++;
+                pandigital.append(i*multiplier);
+            }
+            if (pandigital.length() == 9 && !checkValidPandigital1to9(pandigital)){
+                System.out.println(pandigital + "-" + i);
+                int p = Integer.parseInt(pandigital.toString());
+                if (p > biggest){
+                    biggest = p;
+                    biggestIntGenerated = i;
+                    biggestPandigital = pandigital.toString();
+                }
+            }
+        }
+        System.out.println(biggestPandigital + "-" + biggestIntGenerated);
+        return biggest;
+    }
+
+    private static boolean checkValidPandigital1to9(StringBuilder pandigital)
+    {
+        for (int i = 0; i < pandigital.length(); i++){
+            if (pandigital.charAt(i) == '0')
+                return true;
+            for (int j = i+1; j < pandigital.length(); j++){
+                if (pandigital.charAt(i) == pandigital.charAt(j))
+                    return true;
+            }
+        }
+        return false;
     }
 }
