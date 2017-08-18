a8927150af3e7e2d39cecdd661b99b3ed2c4949c
Anderson Yamamoto <attogba@gmail.com>
2017-08-17 21:31:56 -0300

    Quicksort

--- src/com/atto/challenges/sort/QuickSort.java
+++ src/com/atto/challenges/sort/QuickSort.java
@@ -2,24 +2,43 @@ package com.atto.challenges.sort;
 
 import java.util.Arrays;
 
-public class QuickSort {
-    public static void main(String[] args) {
-        int[] array = new int[] { 23, 61, 63, 15, 73, 36, 3, 67, 11, 63, 58, 52, 15, 99, 45, 16, 8, 63 };
-        System.out.println("Before:" + Arrays.toString(array));
-        sort(array, 0, array.length);
-        System.out.println("After:" + Arrays.toString(array));
+public class QuickSort
+{
+    public static void main(String[] args)
+    {
+        int[] array = new int[] { 23, 61, 63, 15, 73, 36, 3, 67, 11, 63, 58,
+                52, 15, 99, 45, 16, 8, 63 };
+        System.out.println(Arrays.toString(array));
+        sort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
     }
 
-    public static void sort(int[] array, int beginning, int length) {
-        if (length > 1) {
-            int breakingPoint = pivot(array, beginning, length);
-            sort(array, beginning, breakingPoint);
-            sort(array, beginning + breakingPoint, length - breakingPoint);
+    public static void sort(int[] array, int beginning, int end) {
+        if (beginning >= end) return;
+        int left = beginning, right = end;
+        int pivot = array[(beginning + end)/2];
+        System.out.println("Pivot:" + pivot + " \t"+ beginning + "_" + end );
+        while (left <= right){
+            while (array[left] < pivot) left++;
+            while (array[right] > pivot) right--;
+            
+            if (left <= right){
+                System.out.println("switching " + array[left] + "-" + array[right]);
+                switchPosition(array, left, right);
+                left++;
+                right--;
+            }
+            System.out.println(Arrays.toString(array));
         }
+        
+        sort(array, beginning, right);
+        sort(array, left, end);
     }
 
-    private static int pivot(int[] array, int beginning, int length) {
-        
-        return beginning;
+    private static void switchPosition(int[] array, int up, int down)
+    {
+        int temp = array[up];
+        array[up] = array[down];
+        array[down] = temp;
     }
 }
